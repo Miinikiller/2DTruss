7684beee7d9ee16947820fe468650049dc0e36d8
Kristian Åland <kristaal@stud.ntnu.no>
2018-03-14 16:43:47 +0100

    added pointload and boundaryconditions

---
+++ TwoDTrussCalculation/TwoDTrussCalculation/BoundaryConditions.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+using TwoDTrussCalculation.Properties;
+
+namespace TwoDTrussCalculation
+{
+    public class BoundaryConditions : GH_Component
+    {
+
+        public BoundaryConditions()
+          : base("BoundaryConditions", "BDC",
+              "Set boundary conditions at nodes",
+              "Koala", "2D Truss")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddPointParameter("Points", "P", "Points to apply Boundary Conditions", GH_ParamAccess.list);
+            pManager.AddIntegerParameter("Boundary Conditions", "BDC", "Boundary Conditions x,y,z where 0=clamped and 1=free", GH_ParamAccess.list, new List<int>(new int[] { 0, 0, 0 }));
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("B.Cond.", "BDC", "Boundary Conditions for 2D Truss Calculation", GH_ParamAccess.list);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            //Expected inputs
+            List<Point3d> pointList = new List<Point3d>();          //List of points where BDC is to be applied
+            List<int> BDC = new List<int>();                        //is BDC free? (=clamped) (1 == true, 0 == false)
+            List<string> pointInStringFormat = new List<string>();  //output in form of list of strings
+
+
+            //Set expected inputs from Indata and aborts with error message if input is incorrect
+            if (!DA.GetDataList(0, pointList)) return;
+            if (!DA.GetDataList(1, BDC)) { AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "testing"); return; }
+
+
+            //Preallocate temporary variables
+            string BDCString;
+            int bdcx = 0;
+            int bdcy = 0;
+            int bdcz = 0;
+
+
+            if (BDC.Count == 3) //Boundary condition input for identical conditions in all points. Split into if/else for optimization
+            {
+                bdcx = BDC[0];
+                bdcy = BDC[1];
+                bdcz = BDC[2];
+
+                BDCString = bdcx + "," + bdcy + "," + bdcz;
+
+                for (int i = 0; i < pointList.Count; i++)   //Format stringline for all points (identical boundary conditions for all points)
+                {
+                    pointInStringFormat.Add(pointList[i].X + "," + pointList[i].Y + "," + pointList[i].Z + ":" + BDCString);
+                }
+            }
+            else    //BDCs are not identical for all points
+            {
+                for (int i = 0; i < pointList.Count; i++)
+                {
+                    if (i > (BDC.Count / 3) - 1)  //Are there more points than BDCs given? (BDC always lists x,y,z per point)
+                    {
+                        BDCString = bdcx + "," + bdcy + "," + bdcz; //use values from last BDC in list of BDCs
+                    }
+                    else
+                    {
+                        //retrieve BDC for x,y,z-dir
+                        bdcx = BDC[i * 3];
+                        bdcy = BDC[i * 3 + 1];
+                        bdcz = BDC[i * 3 + 2];
+                        BDCString = bdcx + "," + bdcy + "," + bdcz;
+                    }
+                    pointInStringFormat.Add(pointList[i].X + "," + pointList[i].Y + "," + pointList[i].Z + ":" + BDCString);    //Add stringline to list of strings
+                }
+            }
+            DA.SetDataList(0, pointInStringFormat);
+        } //End of main program
+
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                return Resources.BoundaryCondition; //Setting component icon
+            }
+        }
+
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("0efc7b95-936a-4c88-8005-485398c61a31"); }
+        }
+    }
+}
---
+++ TwoDTrussCalculation/TwoDTrussCalculation/Point Load.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Grasshopper.Kernel;
+using Rhino.Geometry;
+
+namespace TwoDTrussCalculation
+{
+    public class Point_Load : GH_Component
+    {
+
+        public Point_Load()
+          : base("Point_Load", "PL",
+              "Set one or more pointloads on nodes",
+              "Koala", "2D Truss")
+        {
+        }
+
+        protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
+        {
+            pManager.AddPointParameter("Points", "P", "Points to apply load(s)", GH_ParamAccess.list);
+            pManager.AddNumberParameter("Load", "L", "Load originally given i Newtons (N), give one load for all points or list of loads for each point", GH_ParamAccess.list);
+            pManager.AddNumberParameter("angle (xz)", "a", "give angle for load in xz plane", GH_ParamAccess.list, 90);
+            //pManager[2].Optional = true; //Code can run without a given angle (90 degrees is initial value)
+        }
+
+        protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("PointLoads", "PL", "PointLoads formatted for Truss Calculation", GH_ParamAccess.list);
+        }
+
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            //Expected inputs and output
+            List<Point3d> pointList = new List<Point3d>();              //List of points where load will be applied
+            List<double> loadList = new List<double>();
+            List<double> anglexz = new List<double>();                  //Initial xz angle 90
+            List<double> anglexy = new List<double> { 0 };              //Initial xy angle  0
+            List<string> pointInStringFormat = new List<string>();      //preallocate final string output
+
+            //Set expected inputs from Indata
+            if (!DA.GetDataList(0, pointList)) return;
+            if (!DA.GetDataList(1, loadList)) return;
+            DA.GetDataList(2, anglexz);
+
+            //initialize temporary stringline and load vectors
+            string vectorString;
+            double load = 0;
+            double xvec = 0;
+            double yvec = 0;
+            double zvec = 0;
+
+            if (loadList.Count == 1 && anglexz.Count == 1)              //loads and angles are identical for all points 
+            {
+                load = -1 * loadList[0];                                //negativ load for z-dir
+                xvec = Math.Round(load * Math.Cos(anglexz[0] * Math.PI / 180) * Math.Cos(anglexy[0] * Math.PI / 180), 2);
+                yvec = Math.Round(load * Math.Cos(anglexz[0] * Math.PI / 180) * Math.Sin(anglexy[0] * Math.PI / 180), 2);
+                zvec = Math.Round(load * Math.Sin(anglexz[0] * Math.PI / 180), 2);
+
+                vectorString = xvec + "," + yvec + "," + zvec;
+                for (int i = 0; i < pointList.Count; i++)               //adds identical load to all points in pointList
+                {
+                    pointInStringFormat.Add(pointList[i].X + "," + pointList[i].Y + "," + pointList[i].Z + ":" + vectorString);
+                }
+            }
+            else   //loads and angles may be different => calculate new xvec, yvec, zvec for all loads                          
+            {
+                for (int i = 0; i < pointList.Count; i++)
+                {
+                    if (loadList.Count < i)             //if pointlist is larger than loadlist, set last load value in remaining points
+                    {
+                        vectorString = xvec + "," + yvec + "," + zvec;
+                    }
+                    else
+                    {
+                        load = -1 * loadList[i];        //negative load for z-dir
+
+                        xvec = Math.Round(load * Math.Cos(anglexz[i]) * Math.Cos(anglexy[i]), 2);
+                        yvec = Math.Round(load * Math.Cos(anglexz[i]) * Math.Sin(anglexy[i]), 2);
+                        zvec = Math.Round(load * Math.Sin(anglexz[i]), 2);
+
+                        vectorString = xvec + "," + yvec + "," + zvec;
+                    }
+
+                    pointInStringFormat.Add(pointList[i].X + "," + pointList[i].Y + "," + pointList[i].Z + ":" + vectorString);
+                }
+            }
+
+            //Set output data
+            DA.SetDataList(0, pointInStringFormat);
+        }
+
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                // You can add image files to your project resources and access them like this:
+                //return Resources.IconForThisComponent;
+                return Properties.Resources.PointLoad;
+            }
+        }
+
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("f6167454-39ae-4204-bfde-0254a1dc6578"); }
+        }
+    }
+}
